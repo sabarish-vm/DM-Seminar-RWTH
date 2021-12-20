5be01dddcedae43212a4dda2abcd8424b9c3dd16
Sabb95 <sabarish.science@gmail.com>
2021-12-20 10:56:46 +0100

    revised initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,2 +1,13 @@
-!pip install numpy cython 
-cd class_public-3.1.1/ && make clean && make -j
+if [ ! -e "run.log" ] ; then
+    echo "running"
+    #pip install numpy cython 
+    #cd class_public-3.1.1/ && make clean && make -j
+    echo "Initialization complete" >> "run.log" 
+    exit
+    
+fi
+
+
+echo "Already Initialized"
+
+
