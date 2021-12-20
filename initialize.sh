bf6aaf6eaf470f2fefd5fe5e4b39a696cb15a5d1
Sabarish <67097021+Sabb95@users.noreply.github.com>
2021-12-20 11:40:35 +0100

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,7 +1,7 @@
 if [ ! -e "run.log" ] ; then
     echo "running"
-    #pip install numpy cython 
-    #cd class_public-3.1.1/ && make clean && make -j
+    pip install numpy cython 
+    cd class_public-3.1.1/ && make clean && make -j
     echo "Initialization complete" >> "run.log" 
     exit
     
