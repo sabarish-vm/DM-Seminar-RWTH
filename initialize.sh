25ab72d6edbca7a31b9f1c408861591134ea73de
Sabarish <67097021+Sabb95@users.noreply.github.com>
2021-12-20 12:10:03 +0100

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,6 +1,6 @@
 if [ ! -e "run.log" ] ; then
     echo "running"
-    pip install numpy cython 
+    pip install matplotlib numpy cython 
     cd class_public-3.1.1/ && make clean && make -j
     echo "Initialization complete" >> "run.log" 
     exit
