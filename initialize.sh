3f370e1ade4afab6a9c4262ffef177d67bb9b341
Sabarish <67097021+sabarish-vm@users.noreply.github.com>
2024-07-01 14:20:26 +0200

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,4 +1,1 @@
-pip install matplotlib scipy cython numpy 
-pip install aquirdturtle_collapsible_headings
-cd class_public-3.1.1/ && make clean && make -j
-echo "Initialization complete" >> "run.log" 
+
